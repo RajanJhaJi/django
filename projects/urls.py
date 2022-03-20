cc2ba2fd3d210594d5d449ea6a87661803a58ccd
RajanJhaJi <rajan29jha@gmail.com>
2022-03-20 23:29:13 +0530

    adding database with crud operation

--- projects/urls.py
+++ projects/urls.py
@@ -3,5 +3,10 @@ from django.http import HttpResponse
 from . import views
 
 urlpatterns = [
-    path("projects/",views.projects)
+    path("",views.projects,name="projects"),
+    path("projects/add_project",views.createproject,name="addproject"),
+    path("projects/update_project/<str:pk>",views.updateproject,name="updateproject"),
+    path("projects/delete_project/<str:pk>",views.deleteproject,name="deleteproject"),
+    path("project/<str:pk>",views.project,name="project"),
+ 
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from .models import Project
+from .forms import ProjectForm
 
 # Create your views here.
 
 
 def projects(request):
-    return render(request,'projects/projects.html')
+    projectlist = Project.objects.all()
+    # print(f'this is the obect we are getting {object}')
+    return render(request,'projects/projects.html',{"projectslist":projectlist})
+
+
+def project(request,pk):
+    project = Project.objects.get(id=pk)
+    tags = project.tags.all()
+    # print(f'this is the obect we are getting {object}')
+    return render(request,'projects/project.html',{"project":project,"tags":tags})
+
+def createproject(request):
+    form = ProjectForm()
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("projects")
+    # print(f'this is the obect we are getting {object}')
+    return render(request,'projects/project-form.html',{"form":form})
+
+def updateproject(request,pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+    if request.method == "POST":
+        form = ProjectForm(request.POST,instance=project) #WE ADD INSTANCE HERE ALSO TO UPDATE THE PROJECT ELSE IT  WILL CREATE A NEW PROJECT
+        if form.is_valid():
+            form.save()
+            return redirect("projects")
+    # print(f'this is the obect we are getting {object}')
+    return render(request,'projects/project-form.html',{"form":form})
+
+def deleteproject(request,pk):
+    project = Project.objects.get(id=pk)
+    project.delete()
+    return redirect("projects")
+
