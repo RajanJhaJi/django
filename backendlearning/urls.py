fe7c06dfb3325a418c06ff929753e7e82df47a2e
RajanJhaJi <rajan29jha@gmail.com>
2022-03-21 09:23:57 +0530

    adding templates

--- backendlearning/urls.py
+++ backendlearning/urls.py
@@ -18,6 +18,10 @@ from django.urls import path,include
 from django.http import HttpResponse
 
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 def projects(request):
     return HttpResponse("Helloooooo!")
 
@@ -25,3 +29,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('projects.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- projects/views.py
+++ projects/views.py
@@ -21,7 +21,7 @@ def project(request,pk):
 def createproject(request):
     form = ProjectForm()
     if request.method == "POST":
-        form = ProjectForm(request.POST)
+        form = ProjectForm(request.POST,request.FILES)
         if form.is_valid():
             form.save()
             return redirect("projects")
@@ -32,7 +32,7 @@ def updateproject(request,pk):
     project = Project.objects.get(id=pk)
     form = ProjectForm(instance=project)
     if request.method == "POST":
-        form = ProjectForm(request.POST,instance=project) #WE ADD INSTANCE HERE ALSO TO UPDATE THE PROJECT ELSE IT  WILL CREATE A NEW PROJECT
+        form = ProjectForm(request.POST,request.FILES,instance=project) #WE ADD INSTANCE HERE ALSO TO UPDATE THE PROJECT ELSE IT  WILL CREATE A NEW PROJECT
         if form.is_valid():
             form.save()
             return redirect("projects")
